8fba897756f0dab86f73dd536970938cc85e0967
Yefferson Espinoza <yespinoza@digitalrealty.com>
2024-05-27 13:54:56 -0500

    chore: config ts, eslint and prettier on Client side

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { readdirSync } from "fs";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+
+const absolutePathAliases: Record<string, string> = {};
+
+const srcPath = path.resolve("src");
+
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map((directory) =>
+  directory.name.replace(/(\.js){1}(x?)/, ""),
+);
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory);
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      ...absolutePathAliases,
+    },
+  },
+});
