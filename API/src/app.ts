94a2fcbbdeb79759ace6c3be95192f3748e7526a
Rodrigo <rodrigogeorgetti.cba@gmail.com>
2024-06-12 09:43:46 -0300

    correcion sobre app.ts

--- API/src/app.ts
+++ API/src/app.ts
@@ -10,6 +10,20 @@ const app = express();
 const HttpResponse = new httpResponse();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.get('/example', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return HttpResponse.NotFound(res, 'No existe ningun dato');
+    } catch (error) {
+      return HttpResponse.NotFound(res, error);
+    }
+  }
+  );
+  app.use(errorHandler);
+  
+  app.use(express.json())
+  
+  app.use(express.urlencoded({extended: true}))
+
 app.use('/', apiRouter);
 
 export default app;
