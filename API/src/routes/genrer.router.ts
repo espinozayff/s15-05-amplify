0975e5a676c0a2c47db3b8ad1b94dfcca7e9815f
Tomas <ravetomas@gmail.com>
2024-06-12 09:14:06 +0200

    Update readme and users schema in api

--- API/src/routes/genrer.router.ts
+++ API/src/routes/genrer.router.ts
@@ -2,11 +2,10 @@ import {Router} from "express";
 import multer from "multer";
 const router = Router();
 import genrerController from "../controllers/genrer.controller";
-import verifyToken from "../middleware/auth";
 
 const upload = multer({ dest: "uploads/" });
 
-router.get('/', verifyToken, genrerController.getGenrer);
+router.get('/',  genrerController.getGenrer);
 router.get('/:id', genrerController.getGenrerById);
 router.post('/', upload.single('image'), genrerController.createGenrer);
 router.put('/:id', genrerController.updateGenrer);
